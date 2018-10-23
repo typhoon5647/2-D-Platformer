5083d7ef3e61d9db4997002e90fba09b1a3d43ac
Householder_Christopher <housc406@hsd.k12.or.us>
2018-10-23 13:03:20 -0700

    added a max speed and skybox

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    private Rigidbody m_rb;
+    public float speed = 10.0F;
+    public float max_speed = 12.0F;
 	// Use this for initialization
 	void Start () {
-		
+        m_rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void FixedUpdate(){
+        /*get user input to add a left-right force to our player object*/
+        float movement = Input.GetAxis("Horizontal");
+        m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
+        /*set the velocity so that it will never be greater then 12.0F*/
+        m_rb.velocity = new Vector3(
+            Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
+            m_rb.velocity.y, m_rb.velocity.z
+            );
+
+        
+    }
 }
